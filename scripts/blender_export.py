b569ab1dc1a58e3d1710f7f514d341be6a7c96e1
Embury1 <patrik.sundstrom@gmail.com>
2020-06-17 17:12:06 +0200

    Rotate y to up

--- scripts/blender_export.py
+++ scripts/blender_export.py
@@ -1,10 +1,13 @@
 import bmesh
 import bpy
+from math import radians
+from mathutils import Matrix
 import os
 import struct
 
 os.system('cls')
 filepath = '{}.nnm'.format(os.path.splitext(bpy.data.filepath)[0])
+y_up_rot = Matrix.Rotation(radians(-90.0), 4, 'X')
 
 with open(filepath, 'wb') as file:
     for obj in bpy.context.scene.objects:
@@ -13,12 +16,11 @@ with open(filepath, 'wb') as file:
 
         mesh_name = obj.name.encode('utf-8')
         file.write(struct.pack('<B', len(mesh_name)))
-        print(len(mesh_name))
         file.write(mesh_name)
-        print(mesh_name)
 
         mesh = bmesh.new()
         mesh.from_mesh(obj.data)
+        mesh.transform(y_up_rot)
         bmesh.ops.triangulate(mesh, faces=mesh.faces[:], quad_method='BEAUTY', ngon_method='BEAUTY')
 
         file.write(struct.pack('<H', len(mesh.verts)))
@@ -28,7 +30,6 @@ with open(filepath, 'wb') as file:
                 continue
 
             file.write(struct.pack('<fff', *v.co.xyz))
-            print(*v.co.xyz)
             file.write(struct.pack('<fff', *v.normal))
 
         file.write(struct.pack('<H', len(mesh.faces) * 3))
